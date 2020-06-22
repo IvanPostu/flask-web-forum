f121d68ed6daba8e2c1a35c5cf7c40693d776b41
IvanPostu <ipostu20000127@gmail.com>
2020-06-22 16:22:26 +0300

    A simple blueprint is written.

--- web-application/app.py
+++ web-application/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from posts.blueprint import posts
 
 app = Flask(__name__)
+app.register_blueprint(posts, url_prefix='/blog')
 
 from routes import home  # noqa: E402
 __all__ = ['home']
