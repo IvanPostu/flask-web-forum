1cd71a1a94031ae21f59c7198224d321ef45c4fb
IvanPostu <ipostu20000127@gmail.com>
2020-06-22 15:59:00 +0300

    Added simple html layout.

--- static_libs_builder/gulpfile.js
+++ static_libs_builder/gulpfile.js
@@ -6,12 +6,10 @@ const del = require('del')
 const OUTPUT_FOLDER = 'collected_static_libs'
 
 const js_files = [
-    'node_modules/bootstrap/dist/js/bootstrap.js',
-    'node_modules/bootstrap/dist/js/bootstrap.min.js',
+    'node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+    'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
     'node_modules/jquery/dist/jquery.js',
     'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/popper.js/dist/popper.js',
-    'node_modules/popper.js/dist/popper.min.js',
 ];
 
 const css_files = [
