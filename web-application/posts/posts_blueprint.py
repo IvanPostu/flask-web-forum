6f1141709b5aa6abd47a10ef042365e2bf2caf3e
IvanPostu <ipostu20000127@gmail.com>
2020-06-23 13:46:34 +0300

    Create post.

--- web-application/posts/posts_blueprint.py
+++ web-application/posts/posts_blueprint.py
@@ -1,13 +1,43 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 
 from models import Post, Tag
+from .forms import PostForm
+from app import db
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 
+@posts.route('/create', methods=['GET', 'POST'])
+def create_post():
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+
+        try:
+            post = Post(title=title, body=body)
+            db.session.add(post)
+            db.session.commit()
+        except Exception:
+            print('Something wrong!')
+
+        return redirect(url_for('posts.index'))
+
+    form = PostForm()
+    return render_template('posts/create_post.html', form=form)
+
+
 @posts.route('/')
 def index():
-    posts = Post.query.all()
+
+    q = request.args.get('q')
+
+    if q:
+        posts = Post.query.filter(Post.title.contains(
+            q) | Post.body.contains(q)).all()
+    else:
+        posts = Post.query.order_by(Post.created.desc())
+
     return render_template('posts/index.html', posts=posts)
 
 
